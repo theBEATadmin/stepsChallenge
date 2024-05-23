934ef1b69cb044c8c820b2fa2da5363100496d47
theBEATadmin <158805883+theBEATadmin@users.noreply.github.com>
2024-05-23 19:12:39 +0800

    Back at it challenge - update

--- script/config.js
+++ script/config.js
@@ -1,5 +1,5 @@
 export const LOGIN_ID = `AKfycbwDYzh4gl_CXZRZmgLc4giXiJUpqoVtuaRNQnYJYQ-UPzQ4peFm84ZiEHyeWNV5btMmnQ`;
 export const STEPS_ID = `AKfycbwD0FrSZXSX8mZYgaO_ZtdcOQg7G5YaP_BZXEUem8GeZcnAsfF7jR6Eaf5P1hgICI6EZA`;
 export const TRACKER_ID = `AKfycbyhXv07Yoi1uJRMTxTqjlj2tontIBdBccBokB_PPVhPZ0ELkr6i_POhp3IqkRGJrKw`;
-export const START_DATE = new Date(2024, 1, 12, 0, 0, 0);
+export const START_DATE = new Date(2024, 3, 28, 0, 0, 0);
 export const DATE_TODAY = new Date();

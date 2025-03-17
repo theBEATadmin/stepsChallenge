8f7fc013b0e4b225e4524448472174215ba27fbe
theBEATadmin <158805883+theBEATadmin@users.noreply.github.com>
2025-03-17 17:30:18 +0800

    Update config.js

--- script/config.js
+++ script/config.js
@@ -1,4 +1,4 @@
-export const LOGIN_ID = `AKfycbxSHEZuxhchA61SxS1IGMnV63Jc983gfg4w8H4iU0GAJuPQey6-3X4kpF8Ckn4wnAQ6`;
+export const LOGIN_ID = `AKfycbxSHEZuxhchA61SxS1IGMnV63Jc983gfg4w8H4iU0GAJuPQey6-3X4kpF8Ckn4wnAQ6/`;
 export const STEPS_ID = `AKfycbwD0FrSZXSX8mZYgaO_ZtdcOQg7G5YaP_BZXEUem8GeZcnAsfF7jR6Eaf5P1hgICI6EZA`;
 export const TRACKER_ID = `AKfycbyhXv07Yoi1uJRMTxTqjlj2tontIBdBccBokB_PPVhPZ0ELkr6i_POhp3IqkRGJrKw`;
 export const START_DATE = new Date(2024, 3, 28, 0, 0, 0);
